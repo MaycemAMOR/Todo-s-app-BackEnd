859b44da84d5a026231d77c73b27fee91f674a69
Maycem_AMOR <maycem.amor@gmail.com>
2024-04-22 21:06:51 +0200

    - Enhancing the Hello World Service with a Path Variable.

--- src/main/java/com/mytech/rest/webservices/restfulwebservices/HelloWorldController.java
+++ src/main/java/com/mytech/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -1,9 +1,11 @@
 package com.mytech.rest.webservices.restfulwebservices;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin("http://localhost:4200/")
 @RestController
 public class HelloWorldController {
 
