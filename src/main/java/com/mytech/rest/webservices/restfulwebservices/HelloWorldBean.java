df6c09d0b6619e0307d0832d7b646d9084183299
Maycem_AMOR <maycem.amor@gmail.com>
2024-04-22 18:06:16 +0200

    Enhancing the Hello World Service to return a Bean.

---
+++ src/main/java/com/mytech/rest/webservices/restfulwebservices/HelloWorldBean.java
@@ -0,0 +1,27 @@
+package com.mytech.rest.webservices.restfulwebservices;
+
+public class HelloWorldBean {
+    private String message;
+
+    public HelloWorldBean(String message) {
+        this.message = message;
+    }
+
+    public HelloWorldBean() {
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "HelloWorldBean{" +
+                "message='" + message + '\'' +
+                '}';
+    }
+}
--- src/main/java/com/mytech/rest/webservices/restfulwebservices/HelloWorldController.java
+++ src/main/java/com/mytech/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -7,7 +7,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
 
     @GetMapping("/hello-world")
-    public String helloWorld(){
+    public String helloWorld() {
         return "Hello World";
+    }
+
+    @GetMapping("/hello-world-bean")
+    public HelloWorldBean helloWorldBean() {
+        return new HelloWorldBean("Hello World");
     }
 }
